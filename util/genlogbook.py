791a89e711d0606d443cc17e443d45ffd68cefc4
Cliff Wulfman <cwulfman@princeton.edu>
2016-04-19 18:41:08 -0400

    Cleaned code to generate only right days per month.

--- util/genlogbook.py
+++ util/genlogbook.py
@@ -18,10 +18,11 @@ def gen_calendar(year):
         month = datetime.date(year,m,1)
         print "<div type='month' n='%i'>\n<head><date>%s</date></head>" % (m,month.strftime("%B"))
         for day in c.itermonthdates(year,m):
-            print "<div type='day'>"
-            print "<head><date when-iso='%s'>%s</date></head>" % (day,day)
-            print "\n"
-            print "</div>"
+            if day.month == m:
+                print "<div type='day'>"
+                print "<head><date when-iso='%s'>%s</date></head>" % (day,day)
+                print "\n"
+                print "</div>"
             
         print "</div>"
             
